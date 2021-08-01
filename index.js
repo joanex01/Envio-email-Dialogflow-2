e3e4de086a5de37f6dbfd45000e31f220fdba175
joanex01 <70920904+joanex01@users.noreply.github.com>
2021-08-01 17:37:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+//const bodyParser = require('body-parser')
+const { request } = require('express')
+const {WebhookClient} = require('dialogflow-fulfillment');
+const nodemailer = require('nodemailer');
+
+const app = express()
+//app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.post('/dialogflow-fullfillment', (request, response)=>{
+    dialogflowFullfillment(request, response)
+})
+
+app.listen(port,() =>{
+    console.log(`Listening on port ${port}`)
+})
+
+const dialogflowFullfillment =(request, response) => {
+    const agent = new WebhookClient({request, response})
+    function envio_email(agent){
+        var nodemailer = require('nodemailer');
+        var transporte = nodemailer.createTransport({
+            service: 'Outlook', //servidor a ser usado
+            auth: {
+                user: process.env.user, // dizer qual o usuário
+                pass: process.env.pass // senha da conta
+            }
+        });
+
+        var email = {
+            from: process.env.user, // Quem enviou este e-mail
+            to: request.body.queryResult.parameters['email'], // Quem receberá
+            subject: request.body.queryResult.parameters['assunto'], // Um assunto
+            html: request.body.queryResult.parameters['mensagem'] // O conteúdo do e-mail
+        };
+
+        transporte.sendMail(email, function(error, info){
+            if(error)
+            console.log (error);
+            throw error; // algo de errado aconteceu.
+            console.log('Email enviado! Leia as informações adicionais: '+ info);
+        });
+    
+    }
+    let intentMap = new Map();
+    intentMap.set("envio_email", envio_email)
+    agent.handleRequest(intentMap)
+}
